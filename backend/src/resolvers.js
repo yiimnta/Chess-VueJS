d4fa491c9af4e730c38410585422e99f45ae9d9e
Chuvantinh <chuvantinh1991@gmail.com>
2021-03-01 21:31:31 +0100

    login resolver error , do not fix yet

--- backend/src/resolvers.js
+++ backend/src/resolvers.js
@@ -1,15 +1,15 @@
-import _neo4jgraphql from "neo4j-graphql-js";
+import neo4jgraphql from "neo4j-graphql-js";
 // const neo4jgraphql = _neo4jgraphql;
 
 const resolvers = {
     Query: {
         user(object, params, ctx, resolveInfo) {
-            return _neo4jgraphql(object, params, ctx, resolveInfo);
+            return neo4jgraphql(object, params, ctx, resolveInfo);
         }
     },
     Mutation: {
         login: async(object, params, ctx, resolveInfo) => {
-            return _neo4jgraphql(object, params, ctx, resolveInfo);
+            return neo4jgraphql(object, params, ctx, resolveInfo);
         }
     }
 };
